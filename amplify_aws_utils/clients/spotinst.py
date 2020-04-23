893ce4d9da8584de2dfb56a571f2e45877fa26d4
Alex Oskotsky <aoskotsky@amplify.com>
2020-04-22 22:40:05 -0400

    add spotinst client

---
+++ amplify_aws_utils/clients/spotinst.py
@@ -0,0 +1,248 @@
+"""Contains SpotinstClient class for talking to the Spotinst REST API"""
+import logging
+from typing import Dict, Any, List, Callable, Optional
+
+import requests
+# pylint: disable=redefined-builtin
+from requests.exceptions import Timeout, ConnectionError
+
+from amplify_aws_utils.jitter import Jitter
+
+SPOTINST_API_HOST = 'https://api.spotinst.io'
+logger = logging.getLogger(__name__)
+
+
+class SpotinstClient:
+    """A client for the Spotinst REST API"""
+
+    def __init__(self, token: str, account_id: str):
+        self.token = token
+        self.account_id = account_id
+
+    def create_group(self, group_config: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Create a new Elastigroup
+        :param dict group_config: Config parameters for Elastigroup
+        :return: Elastigroup
+        :rtype: dict
+        """
+        response = self._make_throttled_request(path='aws/ec2/group', data=group_config, method='post')
+        return response['response']['items'][0]
+
+    def update_group(self, group_id: str, group_config: Dict[str, Any]):
+        """
+        Update an existing Elastigroup
+        :param str group_id: Id of group to update
+        :param dict group_config: New group config
+        """
+        self._make_throttled_request(path='aws/ec2/group/%s' % group_id, data=group_config, method='put')
+
+    def get_group(self, group_id: str) -> Dict[str, Any]:
+        """
+        Get group status
+        :param str group_id: Id of Elastigroup to get status info for
+        :return: List of instances in a Elastigroup
+        :rtype: list[dict]
+        """
+        response = self._make_throttled_request(path='aws/ec2/group/%s' % group_id, method='get')
+        return response['response']['items'][0]
+
+    def get_instances_in_group(self, group_id: str):
+        """
+        Get group status
+        :param str group_id: Id of Elastigroup to get status info for
+        :return: List of instances in a Elastigroup
+        :rtype: list[dict]
+        """
+        response = self._make_throttled_request(path='aws/ec2/group/%s' % group_id + '/status', method='get')
+        return response['response']['items']
+
+    def get_groups(self) -> List[Dict[str, Any]]:
+        """
+        Get a list of all Elastigroup
+        :return: Lst of Elastigroups
+        :rtype: list[dict]
+        """
+        return self._make_throttled_request(path='aws/ec2/group', method='get')['response']['items']
+
+    def delete_group(self, group_id: str):
+        """
+        Delete an Elastigroup
+        :param str group_id: Id of group to delete
+        """
+        self._make_throttled_request(path='aws/ec2/group/%s' % group_id, method='delete')
+
+    def roll_group(self, group_id: str, batch_percentage: int, grace_period: int, health_check_type: str):
+        """
+        Spin up a new set of instances and then shutdown the old instances
+        :param str group_id: Id of Elastigroup to perform roll operation on
+        :param int batch_percentage: Percentage of the group to roll at a time
+        :param int grace_period: Amount of time in seconds to wait for instances to pass health checks
+        :param str health_check_type: Type of health check to use. Available options are ELB, TARGET_GROUP,
+                                      MLB, HCS, EC2, NONE
+        """
+        request = {
+            "batchSizePercentage": batch_percentage,
+            "gracePeriod": grace_period,
+            "healthCheckType": health_check_type,
+            "strategy": {
+                "action": "REPLACE_SERVER"
+            }
+        }
+        self._make_throttled_request(path='aws/ec2/group/%s/roll' % group_id, data=request, method='put')
+
+    def get_deployments(self, group_id: str) -> List[Dict[str, Any]]:
+        """
+        Get list of current and past deployments for a Elastigroup
+        :param str group_id:
+        :return list[dict]:
+        """
+        response = self._make_throttled_request(path='aws/ec2/group/%s/roll' % group_id, method='get')
+        deploys = response['response']['items']
+        return sorted(deploys, key=lambda deploy: deploy['createdAt'])
+
+    def get_roll_status(self, group_id: str, deploy_id: str) -> Dict[str, Any]:
+        """
+        Get info about a specific deployment (also called a roll) for a group
+        :param str group_id:
+        :param str deploy_id:
+        :return dict:
+        """
+        response = self._make_throttled_request(
+            path='aws/ec2/group/%s/roll/%s' % (group_id, deploy_id),
+            method='get'
+        )
+        return response['response']['items'][0]
+
+    def get_group_instances_health(self, group_id: str) -> List[Dict[str, Any]]:
+        """
+        Get info about a specific deployment (also called a roll) for a group
+        :param str group_id:
+        :return dict:
+        """
+        response = self._make_throttled_request(
+            path='aws/ec2/group/%s/instanceHealthiness' % group_id,
+            method='get'
+        )
+        return response['response']['items']
+
+    def _make_throttled_request(self, method: str, path: str, params: Dict[str, str] = None,
+                                data: Dict[str, Any] = None) -> Dict[str, Any]:
+        return self._throttle_spotinst_call(self._make_request, method, path, params, data)
+
+    def _make_request(self, method: str, path: str, params: Dict[str, str] = None,
+                      data: Dict[str, Any] = None):
+        """
+        Convenience function for making requests to the Spotinst API.
+
+        :param str method: What HTTP method to use.
+        :param str path: The API endpoint to call. IE: aws/ec2/group
+        :param dict params: Dictionary of query parameters.
+        :param dict data: Body data.
+        :return: The response from the Spotinst API.
+        :rtype: dict
+        """
+        try:
+            params = params or dict()
+            params['accountId'] = self.account_id
+
+            response = requests.request(
+                method=method,
+                url='%s/%s' % (SPOTINST_API_HOST, path),
+                params=params,
+                json=data,
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer %s" % self.token
+                },
+                timeout=60
+            )
+        except (ConnectionError, Timeout) as err:
+            raise SpotinstRateExceededException(
+                "Rate exceeded while calling %s %s: %s" % (method, path, err)
+            )
+
+        if response.status_code == 401:
+            raise SpotinstApiException("Provided Spotinst API token is not valid")
+
+        if response.status_code == 429:
+            raise SpotinstRateExceededException("Rate exceeded while calling %s %s" % (method, path))
+
+        try:
+            ret = response.json()
+        except ValueError:
+            raise SpotinstApiException("Spotinst API did not return JSON response: %s" % response.text)
+
+        if response.status_code != 200:
+            status = ret['response']['status']
+            req_id = ret['request']['id']
+            errors = ret['response'].get('errors') or []
+
+            for error in errors:
+                if error.get('code') in ("Throttling", "RequestLimitExceeded"):
+                    raise SpotinstRateExceededException("Rate exceeded while calling %s %s" % (method, path))
+
+            raise SpotinstApiException(
+                "Unknown Spotinst API error encountered: %s %s. RequestId %s" % (status, errors, req_id)
+            )
+
+        return ret
+
+    def _throttle_spotinst_call(self, fun: Callable, *args, **kwargs):
+        max_time = 5 * 60
+        jitter = Jitter(min_wait=60)  # wait at least 60 seconds because our rate limit resets then
+        time_passed = 0
+
+        while True:
+            try:
+                return fun(*args, **kwargs)
+            except SpotinstRateExceededException:
+                if logging.getLogger().level == logging.DEBUG:
+                    logger.exception("Failed to run %s.", fun)
+
+                if time_passed > max_time:
+                    raise
+
+                time_passed = jitter.backoff()
+
+
+def get_tag_for_spotinst_group(group: Dict[str, Any], key: str) -> Optional[str]:
+    """
+    Get a tag for a Spotinst group by key
+    :param group: Spotinst group dict
+    :param key: Tag key
+    :return:
+    """
+    return spotinst_tags_to_dict(group['compute']['launchSpecification'].get('tags', {})).get(key)
+
+
+def spotinst_tags_to_dict(tags: List[Dict[str, str]]) -> Dict[str, str]:
+    """
+    Converts spotinst tage to dictionaries
+    :param tags: spotinst tages
+    :return: Dictionary that contains spotinst tags
+    """
+    return {
+        tag['tagKey']: tag['tagValue']
+        for tag in tags
+    }
+
+
+def dict_to_spotinst_tags(tag_dict: Dict[str, str]) -> List[Dict[str, str]]:
+    """
+    Converts dictionaries to spotinst tags
+    :param tag_dict: Dictionary that needs to converted
+    :return: Spotinst tags
+    """
+    return [{
+        'tagKey': key,
+        'tagValue': value
+    } for key, value in tag_dict.items()]
+
+
+class SpotinstApiException(Exception):
+    """Raised if Spotinst API problem encountered"""
+
+
+class SpotinstRateExceededException(Exception):
+    """Raised if Spotinst API throttled a request"""
---
+++ test/unit/test_spotinst_client.py
@@ -0,0 +1,251 @@
+"""Module for testing our Spotinst client"""
+
+from unittest import TestCase
+
+from mock import MagicMock, patch
+import requests_mock
+from requests.exceptions import ReadTimeout, ConnectTimeout, ConnectionError
+
+from amplify_aws_utils.clients.spotinst import SpotinstClient, SpotinstRateExceededException
+
+
+class TestSpotinstClient(TestCase):
+    """Class for testing Spotinst Client"""
+
+    def setUp(self):
+        """Pretest setup"""
+        self.session = MagicMock()
+        self.spotinst_client = SpotinstClient("", "")
+
+    @requests_mock.mock()
+    def test_create_group(self, requests):
+        """Test sending create group request"""
+        requests.post('https://api.spotinst.io/aws/ec2/group', json={
+            'response': {
+                'items': [{
+                    'name': 'foo'
+                }]
+            }
+        })
+
+        result = self.spotinst_client.create_group({
+            'group': {
+                'name': 'foo'
+            }
+        })
+
+        self.assertEqual(len(requests.request_history), 1)
+        self.assertEqual(result, {'name': 'foo'})
+
+    @requests_mock.mock()
+    def test_update_group(self, requests):
+        """Test sending update group request"""
+        requests.put('https://api.spotinst.io/aws/ec2/group/sig-5af12785', json={
+            'response': {
+                'items': [{
+                    'group': {
+                        'name:': 'foo'
+                    }
+                }]
+            }
+        })
+
+        self.spotinst_client.update_group('sig-5af12785', {
+            'group': {
+                'name': 'foo'
+            }
+        })
+
+        self.assertEqual(len(requests.request_history), 1)
+
+    @requests_mock.mock()
+    def test_get_groups(self, requests):
+        """Test sending group list request"""
+        requests.get('https://api.spotinst.io/aws/ec2/group', json={
+            'response': {
+                'items': [{
+                    'instanceId': 'i-abcd1234'
+                }]
+            }
+        })
+
+        self.spotinst_client.get_groups()
+
+        self.assertEqual(len(requests.request_history), 1)
+
+    @requests_mock.mock()
+    def test_delete_group(self, requests):
+        """Test sending delete group request"""
+        requests.delete('https://api.spotinst.io/aws/ec2/group/sig-5af12785', json={
+            "request": {
+                "id": "4a0d5084-0b41-4255-82e5-d64a8232d7cc",
+                "url": "/aws/ec2/group/sig-5af12785",
+                "method": "DELETE",
+                "time": "2015-06-28T15:52:45.772Z"
+            },
+            "response": {
+                "status": {
+                    "code": 200,
+                    "message": "OK"
+                }
+            }
+        })
+
+        self.spotinst_client.delete_group('sig-5af12785')
+
+        self.assertEqual(len(requests.request_history), 1)
+
+    @requests_mock.mock()
+    def test_roll_group(self, requests):
+        """Test sending roll group request"""
+        requests.put('https://api.spotinst.io/aws/ec2/group/sig-5af12785/roll', json={
+            "request": {
+                "id": "3213e42e-455e-4901-a185-cc3eb65fac5f",
+                "url": "/aws/ec2/group/sig-5af12785/roll",
+                "method": "PUT",
+                "time": "2016-02-10T15:49:11.911Z"
+            },
+            "response": {
+                "status": {
+                    "code": 200,
+                    "message": "OK"
+                },
+                "kind": "spotinst:aws:ec2:group:roll",
+            }
+        })
+
+        self.spotinst_client.roll_group('sig-5af12785', 100, 100, health_check_type='EC2')
+
+        self.assertEqual(len(requests.request_history), 1)
+
+    @requests_mock.mock()
+    def test_get_deployments(self, requests):
+        """Test getting a list of deployments for a group"""
+        requests.get('https://api.spotinst.io/aws/ec2/group/sig-5af12785/roll', json={
+            "request": {
+                "id": "3213e42e-455e-4901-a185-cc3eb65fac5f",
+                "url": "/aws/ec2/group/sig-5af12785/roll",
+                "method": "PUT",
+                "time": "2016-02-10T15:49:11.911Z"
+            },
+            "response": {
+                "items": [
+                    {
+                        "id": "sbgd-c47a527a",
+                        "status": "finished",
+                        "progress": {
+                            "unit": "percent",
+                            "value": 100
+                        },
+                        "createdAt": "2017-05-24T12:12:39.000+0000",
+                        "updatedAt": "2017-05-24T12:19:17.000+0000"
+                    },
+                    {
+                        "id": "sbgd-f789ec37",
+                        "status": "in_progress",
+                        "progress": {
+                            "unit": "percent",
+                            "value": 0
+                        },
+                        "createdAt": "2017-05-24T20:13:37.000+0000",
+                        "updatedAt": "2017-05-24T20:15:17.000+0000"
+                    }],
+                "count": 2
+            }
+        })
+
+        deployments = self.spotinst_client.get_deployments('sig-5af12785')
+
+        self.assertEqual(len(requests.request_history), 1)
+        self.assertEqual(len(deployments), 2)
+
+    @requests_mock.mock()
+    def test_get_roll_status(self, requests):
+        """Test getting the status of a deployment"""
+        requests.get('https://api.spotinst.io/aws/ec2/group/sig-5af12785/roll/sbgd-c47a527a', json={
+            "request": {
+                "id": "3213e42e-455e-4901-a185-cc3eb65fac5f",
+                "url": "/aws/ec2/group/sig-5af12785/roll",
+                "method": "PUT",
+                "time": "2016-02-10T15:49:11.911Z"
+            },
+            "response": {
+                "items": [
+                    {
+                        "id": "sbgd-c47a527a",
+                        "status": "finished",
+                        "progress": {
+                            "unit": "percent",
+                            "value": 100
+                        },
+                        "createdAt": "2017-05-24T12:12:39.000+0000",
+                        "updatedAt": "2017-05-24T12:19:17.000+0000"
+                    }],
+                "count": 1
+            }
+        })
+
+        status = self.spotinst_client.get_roll_status('sig-5af12785', 'sbgd-c47a527a')
+
+        self.assertEqual(len(requests.request_history), 1)
+        self.assertEqual(status['status'], 'finished')
+
+    # pylint: disable=unused-argument
+    @requests_mock.mock()
+    @patch("time.sleep", return_value=None)
+    def test_throttle_error(self, requests, sleep_mock):
+        """Test handling spotinst throttling"""
+        requests.get('https://api.spotinst.io/aws/ec2/group', status_code=429)
+
+        self.assertRaises(SpotinstRateExceededException, self.spotinst_client.get_groups)
+
+    # pylint: disable=unused-argument
+    @requests_mock.mock()
+    @patch("time.sleep", return_value=None)
+    def test_timeout_error(self, requests, sleep_mock):
+        """Test handling a request timeout"""
+        requests.get('https://api.spotinst.io/aws/ec2/group', exc=ReadTimeout)
+
+        self.assertRaises(SpotinstRateExceededException, self.spotinst_client.get_groups)
+
+    # pylint: disable=unused-argument
+    @requests_mock.mock()
+    @patch("time.sleep", return_value=None)
+    def test_retry(self, requests, sleep_mock):
+        """Test request keeps retrying until successful"""
+        responses = [
+            {'status_code': 429},
+            {'exc': ReadTimeout},
+            {
+                'status_code': 400,
+                'json': {
+                    'request': {
+                        'id': 'b4415046-bb2d-4338-9b8a-73a405a6fe0c'
+                    },
+                    'response': {
+                        'status': '',
+                        'errors': [{
+                            'message': 'Cant validate AMI',
+                            'code': 'CANT_VALIDATE_IMAGE'
+                        }, {
+                            'message': 'Request limit exceeded',
+                            'code': 'RequestLimitExceeded'
+                        }]
+                    }
+                }
+            },
+            {'exc': ConnectTimeout},
+            {'exc': ConnectionError},
+            {'json': {
+                'response': {
+                    'items': [{
+                        'name': 'foo'
+                    }]
+                }
+            }}
+        ]
+        requests.get('https://api.spotinst.io/aws/ec2/group', responses)
+
+        groups = self.spotinst_client.get_groups()
+
+        self.assertEqual([{'name': 'foo'}], groups)
